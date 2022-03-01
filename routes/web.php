327061c9015446a81c86dae8d21901eb69775aaf
Jean Ballesteros <jeanc.ballesterosz@autonoma.edu.co>
2022-03-01 17:43:09 -0500

    Se modificó la ruta de post

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,10 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/ver/usuario/{user}', [App\Http\Controllers\PostController::class, 'index']);
-Route::get('/ver/posts/{user}', [App\Http\Controllers\PostController::class, 'index']);
+/* Route::get('/ver/usuario/{user}', [App\Http\Controllers\PostController::class, 'index']); */
+Route::get('/posts/{user}', [App\Http\Controllers\PostController::class, 'index']);
+
+Route::resource('posts', App\Http\Controllers\PostController::class)
+    ->except(['index']);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
